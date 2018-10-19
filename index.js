e5550a10159b780cbf3be69812c9b11351ffc02d
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2018-10-19 09:30:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
+
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
+
+
+
+if (index.height === 74) {
+    console.log("The height is correct")
+} else {
+    console.log("Expected: 70, Received: "+index.height)
+}
 
 
 // Don't worry about this
